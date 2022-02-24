7bc325237b1f16b6d91f68ff77e81b5e1fc15b06
Rangerdevv <meerabdul2008@gmail.com>
2022-02-23 19:08:08 -0500

    first page done!

---
+++ main.js
@@ -0,0 +1,14 @@
+const About = document.getElementById("AboutWindow");
+const topbar = document.getElementById("topbar");
+const aboutbtn = document.getElementById("aboutbtn");
+
+aboutbtn.addEventListener("click", function() {
+    About.style.display = "block";
+    topbar.style.display = "none";
+    });
+
+function closeAbout() {
+    About.style.display = "none";
+    topbar.style.display = "block";
+}
+
