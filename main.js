ceb08c0ae48463911c83fb03cbded5e276f12c03
Rangerdevv <meerabdul2008@gmail.com>
2022-02-24 21:18:56 -0500

    Finished the About window

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 const About = document.getElementById("AboutWindow");
 const topbar = document.getElementById("topbar");
 const aboutbtn = document.getElementById("aboutbtn");
+const background = document.getElementById("background");
 
 aboutbtn.addEventListener("click", function() {
     About.style.display = "block";
     topbar.style.display = "none";
+    background.style.backgroundColor = "#0f172a";
+
     });
 
-function closeAbout() {
-    About.style.display = "none";
-    topbar.style.display = "block";
+function closebtn() {
     window.location.reload();
 }
 
--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 
 echo "Updating..."
 git add .
-git commit -m "this is a commit made by the update.sh script"
+git commit -m "Finished the About window"
 git push
 echo "Done!"
