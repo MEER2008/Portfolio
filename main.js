66af39b13ce9da793d317963854f10d34835cf21
Rangerdevv <meerabdul2008@gmail.com>
2022-02-26 21:43:13 -0500

    Projects page done (kinda)

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ const topbar = document.getElementById("topbar");
 const aboutbtn = document.getElementById("aboutbtn");
 const background = document.getElementById("background");
 const projects = document.getElementById("ProjectWindow");
+const skills = document.getElementById("SkillsWindow");
 
 
 function OpenAbout() {
     About.style.display = "block";
     topbar.style.display = "none";
     projects.style.display = "none";
+    skills.style.display = "none";
     window. scrollTo(0,0);
 }
 
@@ -20,6 +22,15 @@ function OpenProjects() {
     projects.style.display = "block";
     topbar.style.display = "none";
     About.style.display = "none";
+    skills.style.display = "none";
+    window. scrollTo(0,0);
+}
+
+function openSkills() {
+    skills.style.display = "block";
+    topbar.style.display = "none";
+    About.style.display = "none";
+    projects.style.display = "none";
     window. scrollTo(0,0);
 }
 
