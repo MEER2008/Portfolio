409db486ce6fdd26488bbc96607d2992abb4a4e6
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2022-12-14 19:42:13 -0500

    added astro

---
+++ src/components/svelte/header.svelte
@@ -0,0 +1,47 @@
+<script>
+        import { LottiePlayer } from '@lottiefiles/svelte-lottie-player';
+  let controlsLayout = [
+    'previousFrame',
+    'playpause',
+    'stop',
+    'nextFrame',
+    'progress',
+    'frame',
+    'loop',
+    'spacer',
+    'background',
+    'snapshot',
+    'zoom',
+    'info',
+  ];
+</script>
+
+<header class=" w-screen h-screen">
+    <div class=" text-4xl text-white text-center pt-40 grid-cols-3 flex flex-row justify-center gap-6">
+        <LottiePlayer
+        src="https://assets7.lottiefiles.com/packages/lf20_3rwasyjy.json"
+        autoplay="{true}"
+        loop="{true}"
+        controls="{false}"
+        renderer="svg"
+        background="transparent"
+        height="{350}"
+        width="{350}"
+        controlsLayout="{controlsLayout}"
+      />
+        <p>Hello ✌️. The name's Ranger. </p>
+        <LottiePlayer
+        src="https://assets10.lottiefiles.com/packages/lf20_g2r05fgw.json"
+        autoplay="{true}"
+        loop="{true}"
+        controls="{false}"
+        renderer="svg"
+        background="transparent"
+        height="{350}"
+        width="{350}"
+        controlsLayout="{controlsLayout}"
+      />
+    </div>
+    <button class=" btn">
+    </button>
+</header>
--- update.sh
+++
@@ -1,6 +0,0 @@
-#user/bin/bash
-echo "Updating..."
-git add .
-git commit -m "Fixed Bugs, typos, and added some quality of life changes"
-git push
-echo "Done!"
