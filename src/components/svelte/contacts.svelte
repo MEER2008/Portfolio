da52487cfc9db10f2863c8f7c3db66c2bd4f3c87
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2022-12-26 19:36:02 -0500

    finished

---
+++ src/components/svelte/contacts.svelte
@@ -0,0 +1,27 @@
+<script>
+
+</script>
+
+<main class="w-screen h-screen">
+    <p class=" text-4xl font-extrabold text-center text-white">Contact Me!</p>
+    <div class="flex flex-row gap-x-5 p-6 justify-center sm:justify-start">
+        <div class="w-16 h-16 bg-slate-800"></div>
+        <p class="text-white text-xl">Deez nutz</p>
+    </div>
+    <div class="flex flex-row gap-x-5 p-6 justify-center sm:justify-start">
+        <div class="w-16 h-16 bg-slate-800"></div>
+        <p class="text-white text-xl">Deez nutz</p>
+    </div>
+    <div class="flex flex-row gap-x-5 p-6 justify-center sm:justify-start">
+        <div class="w-16 h-16 bg-slate-800"></div>
+        <p class="text-white text-xl">Deez nutz</p>
+    </div>
+    <div class="flex flex-row gap-x-5 p-6 justify-center sm:justify-start">
+        <div class="w-16 h-16 bg-slate-800"></div>
+        <p class="text-white text-xl">Deez nutz</p>
+    </div>
+</main>
+
+<style>
+
+</style>
