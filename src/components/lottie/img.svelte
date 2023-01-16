1fe341c6235ecf47222eefa74eb207ad17c2f7bc
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2023-01-15 21:16:01 -0500

    bug fix

---
+++ src/components/lottie/img.svelte
@@ -0,0 +1,31 @@
+<script >
+import { LottiePlayer } from 'node_modules/@lotteries/svelte-lottie-player';
+let controlsLayout = [
+'previousFrame',
+'playpause',
+'stop',
+'nextFrame',
+'progress',
+'frame',
+'loop',
+'spacer',
+'background',
+'snapshot',
+'zoom',
+'info',
+];
+
+export let Link=string;
+</script>
+
+<LottiePlayer
+src={Link}
+autoplay="{true}"
+loop="{true}"
+controls="{false}"
+renderer="svg"
+background="transparent"
+height="{350}"
+width="{350}"
+controlsLayout="{controlsLayout}"
+/>
