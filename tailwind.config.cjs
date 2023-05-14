b41c206fa2238cdff4179bfcf488880161c982b5
RangerDevv <meerabdul2008@gmail.com>
2023-05-14 17:20:03 -0400

    Re doing portfolio for the 3rd time

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		colors: {
-			transparent: 'transparent',
-			current: 'currentColor',
-			'fuzzywuzzy': '#C1666B',
-			'tan': '#D4B483',
-			'timberwolf': '#E4DFDA',
-			'verdigris': '#48A9A6',
-			'liberty': '#4357AD',
-			// 'silver': '#ecebff',
-			// 'bubble-gum': '#ff77e9',
-			// 'bermuda': '#78dcca',
-		  },	  
+	theme: {	  
 		extend: {},
 	},
 	plugins: [
