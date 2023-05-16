555e0b2792c6f2191b0acb3be1ee612ad733d7a2
RangerDevv <meerabdul2008@gmail.com>
2023-05-15 21:16:39 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/edge';
 import svelte from "@astrojs/svelte";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "static",
   adapter: vercel({
     imageService: false
   }),
