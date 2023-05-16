2b5dcbba444c34caaf3412ef84162e443ceec412
RangerDevv <meerabdul2008@gmail.com>
2023-05-15 21:01:59 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({
+    imageService: true
+  }),
   integrations: [svelte(), tailwind()]
 });
